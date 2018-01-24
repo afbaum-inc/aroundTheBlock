5af96c432c1fc72510481ffcd76f3e97c2dc028c
Amanda Baum <amanda.baum01@gmail.com>
2018-01-23 21:57:09 -0500

    adding bootstrap and jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+$('#calc').click(function() {
+  const amount = $('#mortgage-amount').val() - $('#downpayment');
+  const months = $('years').val() * 12;
+  const total = amount / months;
 
+  console.log(total);
+});
