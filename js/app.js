f9e8cd4914d77e6780752ab20fef3ca2132656b2
Amanda Baum <amanda.baum01@gmail.com>
2018-01-27 21:17:25 -0500

    if statements in app.js not working

--- js/app.js
+++ js/app.js
@@ -2,23 +2,38 @@ const length = $('input[name=month-year]:checked').val();
 const percentDollar = $('input[name=amountdown]:checked').val();
 
 
-$('#calc').click(function() {
+function amountDown() {
   //calculate down payment based on percentage or dollar amount entered
   if (percentDollar === 'percent') {
     const total = $('#mortgage-amount').val();
     const downpayment = $('#downpayment').val();
     const p = (downpayment / 100) * total;
+    console.log(p);
+    return p;
   } else {
-    const p = $('#mortgage-amount').val() - $('#downpayment').val();
+    const p = $('#downpayment').val();
+    console.log(p);
+    return p
   }
+}
 
+function paymentLength(){
   if (length === 'year') {
     const n = $('#years').val() * 12;
+    console.log(n);
+    return n;
   } else {
     const n = $('#years').val();
+    console.log(n);
+    return n;
   }
+}
+
+$('#calc').click(function() {
   //calculate the monthly payment on a morgtate amount
   const i = ($('#interest').val().replace(/[^0-9\.]/g, '') / 100) / 12;
+  const n = paymentLength();
+  const p = $('#mortgage-amount').val() - amountDown();
   const x = Math.pow((1 + i ), n);
   const M = ( p * ((i * x) / (x - 1)) ).toFixed(2);
 
